bf01a9d90e340cba43b8d2abc7fe8459e6fcc04f
Ian Canals <canals.ian@gmail.com>
2022-10-08 14:16:47 +0200

    progress made: yes

--- main.cpp
+++ main.cpp
@@ -4,19 +4,26 @@
 #include <map>
 #include <string>
 #include <vector>
-using mojafunkcja_t = std::function<std::string(std::string)>;
+#include "math.h"
+using mojafunkcja_t = std::function<int>;
+
+void wypisz(mojafunkcja_t fun) {
+    using namespace std;
+    cout << "dupa mariana: " << endl;
+}
 
 int main(int argc, char **argv) {
     using namespace std;
-    map<string, mojafunkcja_t> argumenty;
-    argumenty["r"] = [](string x) { return "test"; };
+    map<string, mojafunkcja_t> argument;
+    argument["r"] = [](int x) {  };
 
     try {
         vector<string> argumenty(argv, argv + argc);
         auto selected_f = argumenty.at(1);
+        wypisz(argument.at(selected_f));
     } catch (std::out_of_range aor) {
         cout << "podaj argument. Dostępne to: ";
         cout << endl;
     }
     return 0;
-}test
+}
