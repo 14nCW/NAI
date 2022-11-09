7369213785ee5510679a8e125058df6132952e38
14nCW <75854264+14nCW@users.noreply.github.com>
2022-11-09 16:50:07 +0100

    final

--- main.cpp
+++ main.cpp
@@ -1,58 +1,93 @@
 #include <iostream>
 #include <vector>
-#include <functional>
 #include <random>
-#include <map>
 #include <chrono>
 
-using domain_t = std::vector<double>;
+using namespace std::chrono;
+
 std::random_device rd;
 std::mt19937 mt_generator(rd());
 
-domain_t hill_climbing(const std::function<double(domain_t)> &f, domain_t start_point, std::function<std::vector<domain_t>()> get_close_points, int max_iterations) {
-    domain_t best_p = start_point;
-    for (int iteration = 0; iteration < max_iterations; iteration++) {
-        auto close_points = get_close_points();
-        auto best_neighbour = *std::min_element(close_points.begin(), close_points.end(), [f](auto a, auto b){return f(a) > f(b);});
-        if (f(best_neighbour) < f(best_p)) best_p = best_neighbour;
-    }
-    return best_p;
+auto coords_generator(double r1, double r2) {
+    std::uniform_real_distribution<> coords(r1, r2);
+    return std::pair<double, double>(coords(mt_generator), coords(mt_generator));
 }
 
-int main(int argc, char **argv) {
-    std::map<std::string, std::function<double (std::vector<double>)>> hill_formatery;
+auto random_probe = [](auto f,  int iterations, double r1, double r2) {
+    auto current_p = coords_generator(r1, r2);
+    auto best_point = current_p;
 
-    hill_formatery["sphere"] = [](domain_t x) {
-        return x[0]*x[0];
-    };
-
-    hill_formatery["rastrigin"] = [](domain_t x) {
-        return 10 + (x[0] * x[0] - 10 * (cos(2 * std::numbers::pi * x[0]) * std::numbers::pi/180));
-    };
-
-    hill_formatery["rosenbrock"] = [](domain_t x) {
-        return 100 * pow((x[0] - (x[0]*x[0])),2) + pow((1 - x[0]),2);
-    };
-
-    hill_formatery["styblinskiTang"] = [](domain_t x) {
-        return (pow(x[0],4) - (16 * pow(x[0], 2)) + 5 * x[0])/2;
-    };
-
-    auto get_random_point = [=]() -> domain_t {
-        std::uniform_real_distribution<double>distr(std::stod(argv[2]), std::stod(argv[3]));
-        return {distr(mt_generator), distr(mt_generator)};
-    };
-    auto get_close_points_random = [=]() -> std::vector<domain_t> {
-        std::uniform_real_distribution<double>distr(std::stod(argv[2]),std::stod(argv[3]));
-        return {{distr(mt_generator), distr(mt_generator)}};
-    };
-
-    for (int i = 0; i < 20; i++) {
-        auto start = std::chrono::high_resolution_clock::now();
-        auto best1 = hill_climbing(hill_formatery.at(argv[1]), get_random_point(), get_close_points_random, 1000000);
-        auto finish = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<double> elapsed = finish - start;
-        std::cout << "best x = " << best1[0] << " " << elapsed.count() << std::endl;
+    for (int i = 0; i < iterations; ++i) {
+        if (f(current_p) < f(best_point)) {
+            best_point = current_p;
+        }
+        current_p = coords_generator(r1, r2);
     }
+    return best_point;
+};
+
+int main() {
+
+    auto boothFunc = [](std::pair<double, double> pair) {
+        double x = pair.first, y = pair.second;
+        return pow((x + 2 * y - 7), 2) + pow((2 * x + y - 5), 2);
+    };
+
+    auto matyasFunc = [](std::pair<double, double> pair) {
+        double x = pair.first, y = pair.second;
+        return 0.26 * (pow(x, 2) + pow(y, 2) - 0.48 * x * y);
+    };
+
+    auto treeHumpCamelFunc = [](std::pair<double, double> pair) {
+        double x = pair.first, y = pair.second;
+        return 2 * pow(x, 2) - 1.05 * pow(x, 4) + pow(x, 6) / 6 + x * y + pow(y, 2);
+    };
+
+    for (int i = 1; i <= 20; i++) {
+        auto start = high_resolution_clock::now();
+        auto bestBooth = random_probe(boothFunc, 1000000, -10, 10);
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
+        std::cout   << "x: "
+                    << bestBooth.first
+                    << " y: "
+                    << bestBooth.second
+                    << "Booth: "
+                    << boothFunc(bestBooth)
+                    << " duration: "
+                    << duration.count()
+                    << std::endl;
+    }
+
+    std::cout << "--------------------------------" << std::endl;
+
+    for (int i = 1; i <= 20; i++) {
+        auto start = high_resolution_clock::now();
+        auto bestMatyas = random_probe(matyasFunc, 1000000, -10, 10);
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
+        std::cout   << "Matyas: "
+                    << matyasFunc(bestMatyas)
+                    << " duration: "
+                    << duration.count()
+                    << std::endl;
+
+    }
+
+    std::cout << "--------------------------------" << std::endl;
+
+
+    for (int i = 1; i <= 20; i++) {
+        auto start = high_resolution_clock::now();
+        auto bestTreeHumpCamel = random_probe(treeHumpCamelFunc,  1000000, -5, 5);
+        auto stop = high_resolution_clock::now();
+        auto duration = duration_cast<microseconds>(stop - start);
+        std::cout   << "TreeHumpCamel: "
+                    << treeHumpCamelFunc(bestTreeHumpCamel)
+                    << " duration: "
+                    << duration.count()
+                    << std::endl;
+    }
+
     return 0;
 }
